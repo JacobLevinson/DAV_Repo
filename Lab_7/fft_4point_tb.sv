5969ec1a83a18599a430bfe75446a6af510064bb
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2023-04-08 14:09:27 -0700

    Lab DONE! Finished 16bit fft as well, and also fixed an issue with the testebench not interpreting the output correctly.

--- Lab_7/fft_4point_tb.sv
+++ Lab_7/fft_4point_tb.sv
@@ -9,10 +9,14 @@ reg [31:0] in0;
 reg [31:0] in1;
 reg [31:0] in2;
 reg [31:0] in3;
+reg [15:0] in0_16;
+reg [15:0] in1_16;
+reg [15:0] in2_16;
+reg [15:0] in3_16;
 
 output wire [31:0] out0;
 output wire [31:0] out1;
-output wire[31:0] out2;
+output wire [31:0] out2;
 output wire [31:0] out3;
 output wire [15:0] out0_16;
 output wire [15:0] out1_16;
@@ -30,6 +34,10 @@ initial begin
 	in1 = 32'b00000000110010000000000000000000;
 	in2 = 32'b00000001001011000000000000000000;
 	in3 = 32'b00000001100100000000000000000000;
+	in0_16 = 16'b0000101000000000;
+	in1_16 = 16'b0001010000000000;
+	in2_16 = 16'b0001111000000000;
+	in3_16 = 16'b0010100000000000;
 end
 
 always begin
@@ -38,18 +46,18 @@ always begin
 end
 always @(posedge done) begin
 $display("Finished for 32 bit!");
-$display("Fr 0: imaginary: %d + real %d", $signed(out0[31:16]), $signed(out0[15:0]));
-$display("Fr 1: imaginary: %d + real %d", $signed(out1[31:16]), $signed(out1[15:0]));
-$display("Fr 2: imaginary: %d + real %d", $signed(out2[31:16]), $signed(out2[15:0]));
-$display("Fr 3: imaginary: %d + real %d", $signed(out3[31:16]), $signed(out3[15:0]));
-//$display("Finished for 16 bit!");
-//$display("Fr 0: imaginary: %d + real %d", $signed(out0_16[15:8]), $signed(out0_16[7:0]));
-//$display("Fr 1: imaginary: %d + real %d", $signed(out1_16[15:8]), $signed(out1_16[7:0]));
-//$display("Fr 2: imaginary: %d + real %d", $signed(out2_16[15:8]), $signed(out2_16[7:0]));
-//$display("Fr 3: imaginary: %d + real %d", $signed(out3_16[15:8]), $signed(out3_16[7:0]));
+$display("Fr 0: real: %d + imaginary %d", $signed(out0[31:16]), $signed(out0[15:0]));
+$display("Fr 1: real: %d + imaginary %d", $signed(out1[31:16]), $signed(out1[15:0]));
+$display("Fr 2: real: %d + imaginary %d", $signed(out2[31:16]), $signed(out2[15:0]));
+$display("Fr 3: real: %d + imaginary %d", $signed(out3[31:16]), $signed(out3[15:0]));
+$display("Finished for 16 bit!");
+$display("Fr 0: real: %d + imaginary %d", $signed(out0_16[15:8]), $signed(out0_16[7:0]));
+$display("Fr 1: real: %d + imaginary %d", $signed(out1_16[15:8]), $signed(out1_16[7:0]));
+$display("Fr 2: real: %d + imaginary %d", $signed(out2_16[15:8]), $signed(out2_16[7:0]));
+$display("Fr 3: real: %d + imaginary %d", $signed(out3_16[15:8]), $signed(out3_16[7:0]));
 end
 fft_4pt_32bit UUT1(clk, reset, start, in0, in1, in2, in3,
 	out0, out1, out2, out3, done);
-//fft_4point_16bit UUT2(.clk(clk), .reset(reset), .start(start), .in0(in0[31:16]), .in1(in1[31:16]), .in2(in2[31:16]), .in3(in3[31:16]), 
-//	.out0(out0_16), .out1(out1_16), .out2(out2_16), .out3(out3_16), .done(done_16));
+fft_4pt_16bit UUT2(clk, reset, start, in0_16, in1_16, in2_16, in3_16,
+	out0_16, out1_16, out2_16, out3_16, done_16);
 endmodule
---
+++ Lab_7/fft_4pt_16bit.sv
@@ -0,0 +1,178 @@
+module fft_4pt_16bit(
+	input clk,
+	input rst,
+	input start,
+	input signed [15:0] A,
+	input signed [15:0] B,
+	input signed [15:0] C,
+	input signed [15:0] D,
+	/////////////
+	output reg [15:0] W_out,
+	output reg [15:0] X_out,
+	output reg [15:0] Y_out,
+	output reg [15:0] Z_out,
+	output reg done
+);
+reg [1:0] state;
+reg  [1:0] next_state;
+
+
+localparam RESET = 2'd0;
+localparam STAGE_1 = 2'd1;
+localparam STAGE_2 = 2'd2;
+localparam DONE = 2'd3;
+//
+localparam W_0_2 = 16'b0111111100000000;
+localparam W_0_4 = 16'b0111111100000000;
+localparam W_1_4 = 16'b0000000010000000;
+
+
+////
+reg [15:0] butterfly0_input_0;
+reg [15:0] butterfly0_input_1;
+reg [15:0] butterfly1_input_0;
+reg [15:0] butterfly1_input_1;
+
+wire [15:0] butterfly0_output_0;
+wire [15:0] butterfly0_output_1;
+wire [15:0] butterfly1_output_0;
+wire [15:0] butterfly1_output_1;
+reg [15:0] butterfly0_W;
+reg [15:0] butterfly1_W;
+
+reg [15:0] intermediate_butterfly0_output_0;
+reg [15:0] intermediate_butterfly0_output_1;
+reg [15:0] intermediate_butterfly1_output_0;
+reg [15:0] intermediate_butterfly1_output_1;
+
+// State control
+always @ (posedge clk) begin
+	if(rst == 1) begin
+		state <= RESET;
+	end else begin
+		state <= next_state;
+	end
+	if(state == STAGE_1) begin
+		intermediate_butterfly0_output_0 <= butterfly0_output_0;
+		intermediate_butterfly0_output_1 <= butterfly0_output_1;
+		intermediate_butterfly1_output_0 <= butterfly1_output_0;
+		intermediate_butterfly1_output_1 <= butterfly1_output_1;
+	end else begin
+		intermediate_butterfly0_output_0 <= intermediate_butterfly0_output_0;
+		intermediate_butterfly0_output_1 <= intermediate_butterfly0_output_1;
+		intermediate_butterfly1_output_0 <= intermediate_butterfly1_output_0;
+		intermediate_butterfly1_output_1 <= intermediate_butterfly1_output_1;
+	end
+end
+
+
+
+
+butterfly_unit #(16) butterfly0(butterfly0_input_0, butterfly0_input_1, butterfly0_W, butterfly0_output_0, butterfly0_output_1);
+butterfly_unit #(16) butterfly1(butterfly1_input_0, butterfly1_input_1, butterfly1_W, butterfly1_output_0, butterfly1_output_1);
+
+// Outputs
+always_comb begin
+case(state)
+	RESET: begin
+		if(rst == 1) begin
+			next_state = RESET;
+			W_out = 0;
+			X_out = 0;
+			Y_out = 0;
+			Z_out = 0;
+			done = 0;
+		end else if(start == 1) begin
+			next_state = STAGE_1;
+			W_out = 0;
+			X_out = 0;
+			Y_out = 0;
+			Z_out = 0;
+			done = 0;
+		end else begin
+			next_state = RESET;
+			W_out = 0;
+			X_out = 0;
+			Y_out = 0;
+			Z_out = 0;
+			done = 0;
+		end
+		butterfly0_input_0 = 0;
+		butterfly0_input_1 = 0;
+		butterfly1_input_0 = 0;
+		butterfly1_input_1 = 0;
+		butterfly0_W = 0;
+		butterfly1_W = 0;
+	end
+	STAGE_1: begin
+		next_state = STAGE_2;
+		done = 0;
+		W_out = 0;
+		X_out = 0;
+		Y_out = 0;
+		Z_out = 0;
+		butterfly0_input_0 = A;
+		butterfly0_input_1 = C;
+		butterfly1_input_0 = B;
+		butterfly1_input_1 = D;
+		butterfly0_W = W_0_2;
+		butterfly1_W = W_0_2;
+	end
+	STAGE_2: begin
+		next_state = DONE;
+		done = 0;
+		W_out = 0;
+		X_out = 0;
+		Y_out = 0;
+		Z_out = 0;
+		butterfly0_input_0 = intermediate_butterfly0_output_0;
+		butterfly0_input_1 = intermediate_butterfly1_output_0;
+		butterfly1_input_0 = intermediate_butterfly0_output_1;
+		butterfly1_input_1 = intermediate_butterfly1_output_1;
+		butterfly0_W = W_0_4;
+		butterfly1_W = W_1_4;
+	end
+	DONE: begin
+		next_state = DONE;
+		done = 1;
+		W_out = butterfly0_output_0;
+		X_out = butterfly1_output_0;
+		Y_out = butterfly0_output_1;
+		Z_out = butterfly1_output_1;
+		butterfly0_input_0 = intermediate_butterfly0_output_0;
+		butterfly0_input_1 = intermediate_butterfly1_output_0;
+		butterfly1_input_0 = intermediate_butterfly0_output_1;
+		butterfly1_input_1 = intermediate_butterfly1_output_1;
+		butterfly0_W = W_0_4;
+		butterfly1_W = W_1_4;
+	end
+	default: begin
+		next_state = RESET;
+		done = 0;
+		W_out = 0;
+		X_out = 0;
+		Y_out = 0;
+		Z_out = 0;
+		butterfly0_input_0 = A;
+		butterfly0_input_1 = C;
+		butterfly1_input_0 = B;
+		butterfly1_input_1 = D;
+		butterfly0_W = 0;
+		butterfly1_W = 0;
+	end
+endcase
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
